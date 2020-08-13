1bc369630be5b63aabf1fb810bc6de9f5ca568a9
Scott <spowers.42@gmail.com>
2020-08-13 09:16:05 -0400

    refactor

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os, ast
-from uuid import uuid4 as uuid
 import pandas as pd
 
 dataset = os.environ['INPUT_DATASET']
@@ -7,20 +6,31 @@ target = os.environ['INPUT_TARGET']
 usecase = os.environ['INPUT_USECASE']
 repo = os.environ['GITHUB_REPOSITORY']
 
-dataset_path = f'https://raw.githubusercontent.com/{repo}/master/{dataset}.csv'
-data = pd.read_csv(dataset_path)
-data.head()
 
-if usecase == 'regression':
-    from pycaret.regression import *
-elif usecase == 'classification':
-    from pycaret.classification import *
-else:
-    raise NotImplementedError("The use case {usecase} is not currently supported")
+def load_data() -> pd.DataFrame:
+    dataset_path = f'https://raw.githubusercontent.com/{repo}/master/{dataset}.csv'
+    data = pd.read_csv(dataset_path)
+    data.head()
+    return data
 
-exp1 = setup(data, target=target, session_id=uuid(), silent=True, html=False, log_experiment=True, experiment_name='exp_github')
-best = compare_models()
-best_model = finalize_model(best)
 
-save_model(best_model, 'model')
-logs_exp_github = get_logs(save=True)
+def train(data: pd.DataFrame):
+    if usecase == 'regression':
+        from pycaret.regression import *
+    elif usecase == 'classification':
+        from pycaret.classification import *
+    else:
+        raise NotImplementedError("The use case {usecase} is not currently supported")
+
+    exp1 = setup(data, target=target, session_id=123, silent=True, html=False, log_experiment=True, experiment_name='exp_github')
+    best = compare_models()
+    best_model = finalize_model(best)
+
+    save_model(best_model, 'model')
+    logs_exp_github = get_logs(save=True)
+    return best_model
+
+
+def main():
+    data = load_data()
+    best_model = train(data)
