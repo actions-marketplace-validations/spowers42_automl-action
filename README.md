dbc28e8cb688bb8ed43686351880449b8ad1f3bb
Scott <spowers.42@gmail.com>
2020-08-13 06:48:13 -0400

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # automl-action
 A simple autoML system using pycaret that runs in github actions
+
+This is based on the tutorial in (TDS)[git@github.com:spowers42/automl-action.git] as well as (FastAI)[https://www.fast.ai/2018/07/16/auto-ml2/].  It is primarily my own experimentation to explore my own home brew automl along with docker and github actions.
---
+++ app.py
@@ -0,0 +1,26 @@
+import os, ast
+from uuid import uuid
+import pandas as pd
+
+dataset = os.environ['INPUT_DATASET']
+target = os.environ['INPUT_TARGET']
+usecase = os.environ['INPUT_USECASE']
+repo = os.environ['GITHUB_REPOSITORY']
+
+dataset_path = f'https://raw.githubusercontent.com/{repo}/master/{INPUT_DATASET}.csv'
+data = pd.read_csv(dataset_path)
+data.head()
+
+if usecase == 'regression':
+    from pycaret.regression import *
+elif usecase == 'classification':
+    from pycaret.cassification import *
+else:
+    raise NotImplementedError("The use case {usecase} is not currently supported")
+
+exp1 = setup(data, target=target, session_id=uuid(), silent=True, html=False, log_experiment=True, experiment_name='exp_github')
+best = compare_models()
+best_model = finalize_model(best)
+
+save_model(best_model, 'model')
+logs_exp_github = get_logs(save=True)
